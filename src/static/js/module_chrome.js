1813dfbb57b132e51dc66ebe04d780517878d95d
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-15 13:37:29 -0600

    Fixed hotspot issue for module tab.

--- src/static/js/module_chrome.js
+++ src/static/js/module_chrome.js
@@ -2,14 +2,24 @@ var device_controller = require('./test_device_controller');
 var module_manager = require('./module_manager');
 
 var MODULE_TAB_CONTAINER = '#module-list';
+var MODULE_TAB_CLASS = 'module-tab';
+var MODULE_TAB_ID_POSTFIX = '-module-tab';
+
+
+function selectModule(name)
+{
+    $('.' + MODULE_TAB_CLASS).removeClass('selected');
+    console.log('#' + name + MODULE_TAB_ID_POSTFIX);
+    $('#' + name + MODULE_TAB_ID_POSTFIX).addClass('selected');
+}
 
 
 function addModuleTab(targetElement, module)
 {
-    var linkID = module.name + '-module-tab';
+    var tabID = module.name + MODULE_TAB_ID_POSTFIX;
     $(targetElement).append(
-        $('<li>').append(
-            $('<span>').attr('class', 'tab').append(module.humanName)
+        $('<li>').attr('class', MODULE_TAB_CLASS).attr('id', tabID).html(
+            module.humanName
         )
     );
 }
@@ -18,6 +28,10 @@ function addModuleTab(targetElement, module)
 function displayActiveModules(activeModules)
 {
     activeModules.forEach(function(e){addModuleTab(MODULE_TAB_CONTAINER, e);});
+    $('.' + MODULE_TAB_CLASS).click(function(event){
+        console.log(event.target.id);
+        selectModule(event.target.id.replace(MODULE_TAB_ID_POSTFIX, ''));
+    });
 }
 
 
