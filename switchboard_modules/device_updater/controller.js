c09d54199b61411e6aed1d78f45ac25c53af5470
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-18 13:11:30 -0600

    Added firmware listing for switchboard modules.

--- switchboard_modules/device_updater/controller.js
+++ switchboard_modules/device_updater/controller.js
@@ -1,5 +1,7 @@
 var DEVICE_SELECTOR_SRC = 'device_updater/device_selector.html';
+var FIRMWARE_LISTING_SRC = 'device_updater/firmware_listing.html';
 var DEVICE_SELECTOR_PANE_SELECTOR = '#device-overview';
+var FIRMWARE_LIST_SELECTOR = '#firmware-list';
 
 
 function UpgradeableDeviceAdapter(device)
@@ -26,6 +28,22 @@ function UpgradeableDeviceAdapter(device)
 }
 
 
+function getAvailableFirmwareListing(onError, onSuccess)
+{
+    var firmwareListing = [
+        {'version': '1.24', 'latest': true},
+        {'version': '1.23', 'latest': false},
+        {'version': '1.22', 'latest': false},
+        {'version': '1.21', 'latest': false},
+        {'version': '1.20', 'latest': false}
+    ];
+
+    window.setTimeout(function(){
+        displayFirmwareListing(firmwareListing);
+    }, 2000);
+}
+
+
 function onChangeSelectedDevices()
 {
     var selectedCheckboxes = $('.device-selection-checkbox:checked');
@@ -33,6 +51,47 @@ function onChangeSelectedDevices()
         $('#device-configuration-pane').fadeOut();
     else
         $('#device-configuration-pane').fadeIn();
+}
+
+
+function onFirmwareLinkSelect(event)
+{
+    var firmwareDisplayStr;
+
+    var version = event.target.id.replace('-selector', '');
+
+    if(event.target.getAttribute('latest') === 'true')
+        firmwareDisplayStr = version + ' (latest)';
+    else
+        firmwareDisplayStr = version;
+
+    $('#selected-firmware').html(firmwareDisplayStr);
+    $('#selected-firmware').attr('selected-version', version);
+}
+
+
+function displayFirmwareListing(firmwareInfo)
+{
+    var latestFirmwares = firmwareInfo.filter(function(e){ return e.latest; });
+    var latestFirmware = latestFirmwares[0];
+
+    $('#selected-firmware').html(latestFirmware.version + ' (latest)');
+    $('#selected-firmware').attr('selected-version', latestFirmware.version);
+
+    var location = fs_facade.getExternalURI(FIRMWARE_LISTING_SRC);
+    fs_facade.renderTemplate(
+        location,
+        {'firmwares': firmwareInfo},
+        genericErrorHandler,
+        function(renderedHTML)
+        {
+            $(FIRMWARE_LIST_SELECTOR).html(renderedHTML);
+            $('.firmware-selection-link').click(onFirmwareLinkSelect);
+            
+            $('#web-load-waiting-indicator').hide();
+            $('#firmware-select').fadeIn();
+        }
+    );
 }
 
 
@@ -55,4 +114,6 @@ $('#network-configuration').ready(function(){
             $('.device-selection-checkbox').click(onChangeSelectedDevices);
         }
     );
+
+    getAvailableFirmwareListing(genericErrorHandler, displayFirmwareListing);
 });
