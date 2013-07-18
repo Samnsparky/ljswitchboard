c908b7ac29f3c10dd923952eab87d8efc78fc4f7
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-18 11:32:30 -0600

    Shell of network configuration added.

--- switchboard_modules/network_configuration/controller.js
+++ switchboard_modules/network_configuration/controller.js
@@ -1,5 +1,5 @@
 var DEVICE_SELECTOR_SRC = 'network_configuration/device_selector.html';
-var DEVICE_SELECTOR_PANE_SELECTOR = '#device-configuration-pane';
+var DEVICE_SELECTOR_PANE_SELECTOR = '#device-overview';
 
 
 function DeviceNetworkAdapter(device)
@@ -7,23 +7,70 @@ function DeviceNetworkAdapter(device)
     this.getSerial = function()
     {
         return device.getSerial();
-    }
+    };
+
     this.getName = function()
     {
         return device.getName();
-    }
+    };
+
     this.getIPAddress = function()
     {
         return '192.168.0.1';
-    }
+    };
+
     this.getConnectionStatus = function()
     {
         return true;
-    }
+    };
+
     this.getNetwork = function()
     {
         return 'testnetwork';
-    }
+    };
+
+    this.getNetworkPassword = function()
+    {
+        return 'testnetworkpassword';
+    };
+
+    this.getSubnet = function()
+    {
+        return '255.255.255.0';
+    };
+
+    this.getDefaultDNS = function()
+    {
+        return '8.8.8.8';
+    };
+
+    this.getAltDNS = function()
+    {
+        return '8.8.4.4';
+    };
+}
+
+
+function onChangeSelectedDevices()
+{
+    $('#device-configuration-pane').hide();
+
+    var selectedCheckboxes = $('.device-selection-checkbox:checked');
+    if(selectedCheckboxes.length == 0)
+        return;
+
+    var keeper = device_controller.getDeviceKeeper();
+    var deviceSerial = selectedCheckboxes[0].id.replace('-selector', '');
+    var device = new DeviceNetworkAdapter(keeper.getDevice(deviceSerial));
+
+    $('#network-name-input').val(device.getNetwork());
+    $('#network-password-input').val(device.getNetworkPassword());
+    $('#default-ip-input').val(device.getIPAddress());
+    $('#default-subnet-input').val(device.getSubnet());
+    $('#default-dns-input').val(device.getDefaultDNS());
+    $('#alt-dns-input').val(device.getAltDNS());
+
+    $('#device-configuration-pane').fadeIn();
 }
 
 
@@ -43,6 +90,7 @@ $('#network-configuration').ready(function(){
         function(renderedHTML)
         {
             $(DEVICE_SELECTOR_PANE_SELECTOR).html(renderedHTML);
+            $('.device-selection-checkbox').click(onChangeSelectedDevices);
         }
     );
 });
