d6689ddc0a6f5fa6fddb33ffa6ecf0d45caa1422
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-18 21:17:29 -0600

    Styling for analog outputs config.

--- switchboard_modules/analog_outputs/controller.js
+++ switchboard_modules/analog_outputs/controller.js
@@ -1,5 +1,9 @@
 var sprintf = require('sprintf-js');
 
+var CONFIG_PANE_SELECTOR = '#configuration-pane';
+var OUTPUTS_TEMPLATE_SRC = 'analog_outputs/output_controls.html';
+var OUTPUTS_DATA_SRC = 'analog_outputs/outputs.json';
+
 
 function formatVoltageTooltip(value)
 {
@@ -7,6 +11,29 @@ function formatVoltageTooltip(value)
 }
 
 
-$('#analog-output-config').ready(function(){
+function createSliders()
+{
     $('.slider').slider({formater: formatVoltageTooltip, value: 0});
+}
+
+
+$('#analog-output-config').ready(function(){
+    var templateLocation = fs_facade.getExternalURI(OUTPUTS_TEMPLATE_SRC);
+    var outputsSrc = fs_facade.getExternalURI(OUTPUTS_DATA_SRC);
+
+    fs_facade.getJSON(outputsSrc, genericErrorHandler, function(outputsInfo){
+        fs_facade.renderTemplate(
+            templateLocation,
+            {'outputs': outputsInfo},
+            genericErrorHandler,
+            function(renderedHTML)
+            {
+                $(CONFIG_PANE_SELECTOR).hide(function(){
+                    $(CONFIG_PANE_SELECTOR).html(renderedHTML);
+                    $(CONFIG_PANE_SELECTOR).fadeIn();
+                    createSliders();
+                });
+            }
+        );
+    });
 });
