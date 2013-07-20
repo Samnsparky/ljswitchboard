90a8df21f6e13e8f8825867cb0237dc1d88560be
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-19 23:05:42 -0600

    Updated inline docs for register matrix logic.

--- switchboard_modules/register_matrix/controller.js
+++ switchboard_modules/register_matrix/controller.js
@@ -1,3 +1,12 @@
+/**
+ * Logic for the register matrix LabJack Switchboard module.
+ *
+ * Logic for a matrix with information about registers that also allows users
+ * to read and write the current value of those registers via raw values.
+ *
+ * @author A. Samuel Pottinger (LabJack Corp, 2013)
+**/
+
 var async = require('async');
 var handlebars = require('handlebars');
 var sprintf = require('sprintf-js');
@@ -29,6 +38,15 @@ var DATA_TYPE_SIZES = {
 
 
 // TODO: This is implementing a subset of LJMMM
+/**
+ * Enumerates / interprets an LJMMM field.
+ *
+ * @param {String} name The field to interpret as an LJMMM string.
+ * @param {function} onSuccess The function to call after the LJMMM field has
+ *      been interpreted. The callback should take a single argument which
+ *      will be an Array of String or, in other words, the expansion / result of
+ *      the interpretation of the LJMMM field.
+**/
 function expandLJMMMName(name, onSuccess)
 {
     var ljmmmRegex = /^(.*)\#\((\d+)\:(\d+)\:?(\d+)?\)(.*)$/;
@@ -55,14 +73,35 @@ function expandLJMMMName(name, onSuccess)
 }
 
 
+/**
+ * Get the size of the data of a given type in registers.
+ *
+ * Determine the size of the data of a given data type as indicated by a type
+ * name. The size will be reported in MODBUS registers, each of which is two
+ * bytes. Unknown data types are returned as -1 registers.
+ *
+ * @param {String} typeName The name of the type to get the data type size for.
+ * @return {Number} The number of registers that values of the given data type 
+ *      take up. -1 is returned if the type could not be found.
+**/
 function getTypeRegSize(typeName)
 {
     if(DATA_TYPE_SIZES[typeName] === undefined)
-        return 1;
+        return -1;
     return DATA_TYPE_SIZES[typeName];
 }
 
 
+/**
+ * Interpret an entry's name field as LJMMM, enumerating as appropriate.
+ *
+ * @param {Object} entry An Object containing information about a register or
+ *      set of registers.
+ * @param {function} onSuccess The function to call after enumerating.
+ * @return {Array} An Array of Object that results from interpreting the name
+ *      of the provided entry as an LJMMM field, enumerating and creating the
+ *      appropriate entries when interpreting that field.
+**/
 function expandLJMMMEntry(entry, onSuccess)
 {
     var expandedEntries = expandLJMMMName(entry.name, function(names){
@@ -88,6 +127,20 @@ function expandLJMMMEntry(entry, onSuccess)
 }
 
 
+/**
+ * Interpret the name fields of entries as LJMMM fields.
+ *
+ * Interpret the name fields of entries as LJMMM fields, creating the
+ * appropriate register information Objects during enumeration during that
+ * LJMMM interpretation.
+ *
+ * @param {Array} entries An Array of Object with information about registers
+ *      whose name field should be interpreted as LJMMM fields.
+ * @return {q.deferred.promise} A Q promise that resolves to an Array of Array
+ *      of Objects with information about registers. Each sub-array is the
+ *      result of interpreting a register entry's name field as LJMMM and
+ *      enumerating as appropriate.
+**/
 function expandLJMMMEntries(entries)
 {
     var deferred = q.defer();
@@ -108,6 +161,14 @@ function expandLJMMMEntries(entries)
 }
 
 
+/**
+ * Load information about registers for all devices.
+ *
+ * @return {q.defer.promise} A Q promise that will resolve to an Array of Object
+ *      where each object contains information about a register or set of
+ *      registers. The later will have a name field that can be interpreted as
+ *      LJMMM.
+**/
 function getRegisterInfo()
 {
     var deferred = q.defer();
@@ -121,6 +182,20 @@ function getRegisterInfo()
 }
 
 
+/**
+ * Filter out register entries that are not available on the given device type.
+ *
+ * @param {Array} registers An Array of Object with information about a
+ *      register or a set of registers. Each Object must have a device field
+ *      with the type of Array of Object, each element having a name field.
+ * @param {String} deviceName The device type to look for. All register entries
+ *      that do not have this device type will be filtered out.
+ * @return {q.defer.promise} A Q promise that will resolve to an Array of Object
+ *      where each Object contains information about an register or class of
+ *      registers. This Array will contain all of the registers originally
+ *      passed in that have the given device type listed in their devices
+ *      field. All others will be excluded.
+**/
 function filterDeviceRegisters(registers, deviceName)
 {
     var deferred = q.defer();
@@ -145,6 +220,15 @@ function filterDeviceRegisters(registers, deviceName)
 }
 
 
+/**
+ * Create a function as a closure over a device type for filterDeviceRegisters.
+ *
+ * Create a closure around device that calls filterDeviceRegisters with the
+ * provided device type.
+ *
+ * @param {String} device The device type that is being filtered for.
+ * @return {function} Closure with device type info. See filterDeviceRegisters.
+**/
 function createDeviceFilter(device)
 {
     return function(registers){
@@ -153,6 +237,21 @@ function createDeviceFilter(device)
 }
 
 
+/**
+ * Add a new field to the given register information objects with firmware info.
+ *
+ * Add a new field to the given register information objects with the minimum
+ * firmware at which the cooresponding register became available for the given
+ * device type.
+ *
+ * @param {Array} registers An Array of Object with information about registers
+ *      to decorate.
+ * @param {String} device The name of the device type to find the minimum
+ *      firmware version for.
+ * @return {q.defer.promise} A Q promise that resovles to an Array of Object
+ *      with information about a register or class of registers. These modified
+ *      Objects will have an added relevantFwmin field.
+**/
 function fwminSelector(registers, device)
 {
     var deferred = q.defer();
@@ -181,6 +280,16 @@ function fwminSelector(registers, device)
 }
 
 
+/**
+ * Create a closure around device type information for fwminSelector.
+ *
+ * Create a closure around device type information to call fwminSelector for
+ * that device type.
+ *
+ * @param {String} device The device type to create the closure with.
+ * @return {function} Closure around fwminSelector for the given device type.
+ *      See fwminSelector.
+**/
 function createFwminSelector(device)
 {
     return function(registers){
@@ -189,6 +298,11 @@ function createFwminSelector(device)
 }
 
 
+/**
+ * jQuery event listener to show / hide documentation for a register entry.
+ *
+ * @param {Event} event Standard jQuery event information.
+**/
 function toggleRegisterInfo(event)
 {
     var toggleButtonID = event.target.id;
@@ -216,6 +330,14 @@ function toggleRegisterInfo(event)
 }
 
 
+/**
+ * jQuery event listener to add a register to the list of registers to watch.
+ *
+ * jQuery event listener to add a register to the list of registers to watch or
+ * manipulate.
+ *
+ * @param {Event} event Standard jQuery event information Object.
+**/
 function addRegisterToWatchList(event)
 {
     var buttonID = event.target.id;
@@ -225,6 +347,15 @@ function addRegisterToWatchList(event)
 }
 
 
+/**
+ * Render a table with information about registers.
+ *
+ * Render the UI widgets to view / manipulate information about device
+ * registers.
+ *
+ * @param {Array} entries An Array of Object with information about registers.
+ * @return {q.defer.promise} A Q promise that resolves to null.
+**/
 function renderRegistersTable(entries)
 {
     var deferred = q.defer();
@@ -251,6 +382,16 @@ function renderRegistersTable(entries)
 }
 
 
+/**
+ * Turn a hierarchical Array of register information into a linear one.
+ *
+ * Convert an Array with Array elements containing Objects with register
+ * information to an Array of the same Objects.
+ *
+ * @param {Array} entries The Array of Arrays to convert.
+ * @return {q.defer.promise} A Q promise that resolves to the "flattened" or
+ *      converted Array of Object.
+**/
 function flattenEntries(entries)
 {
     var deferred = q.defer();
@@ -272,6 +413,19 @@ function flattenEntries(entries)
 }
 
 
+/**
+ * Convert the tags attribute of Objects with register info to a String.
+ *
+ * Convert the tags attribute of Objects with register info from an Array of
+ * String tags to a String containing the same list of tags joined by a comma.
+ * The list will be saved as a new attribute called flatTagStr on the same
+ * objects.
+ *
+ * @param {Array} registers An Array of Objects with register information to
+ *      create flattened tag strings for.
+ * @return {q.defer.promise} A Q promise that resolves to the new Array of
+ *      Object with flattened tag strings.
+**/
 function flattenTags(registers)
 {
     var deferred = q.defer();
